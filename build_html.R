a5f2d0399d7592fb841af24f705ca66c8c791e54
Tim Hosgood <timhosgood@gmail.com>
2023-06-28 00:26:48 +0200

    trying bs4

--- build_html.R
+++ build_html.R
@@ -1,5 +1,9 @@
 #!/usr/bin/env Rscript
 
+# bookdown::render_book("index.Rmd",
+#                       "bookdown::gitbook",
+#                       config_file = "_bookdown.yml")
+
 bookdown::render_book("index.Rmd",
-                      "bookdown::gitbook",
+                      "bookdown:bs4_book",
                       config_file = "_bookdown.yml")
